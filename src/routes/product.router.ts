f95095d35afcc1e7ca691ea798632777fb5ef3f8
Cristiane Kizelevicius <cristiane@samaan.com.br>
2023-02-07 13:57:34 -0300

    finaliza req 5

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,11 +1,12 @@
-import { Router } from 'express';
+import { Router } from 'express'; 
 import ProductsController from '../controller/products.controller';
 
 const router = Router();
 const productsController = new ProductsController();
 
 router.post('/', productsController.adicProducts);
-router.get('/', (req, res) => productsController.findAllProduct(req, res)); // monitoria vallin 26:02 - chamando req-res - chamar o metodo corretamente para nao perder o this. tipo pequeno middelware
+router.get('/', (req, res) => productsController.findAllProduct(req, res)); // monitoria vallin 26:02  video 2
+// chamando req-res - chamar o metodo corretamente para nao perder o this. tipo pequeno middelware
 router.get('/:id', (req, res) => productsController.findOneProduct(req, res));
 
 export default router;
