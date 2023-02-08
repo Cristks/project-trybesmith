690901916713a32570602de9124af9337787b1d7
Cristiane Kizelevicius <cristiane@samaan.com.br>
2023-02-07 21:20:32 -0300

    finaliza req 7

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import UserController from '../controller/user.controller';
+import userMiddleware from '../middlewares/user.middleware';
 
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.post('/', userController.adicUser);
+userRouter.post(
+  '/', 
+  userMiddleware.verifUserName,
+  userMiddleware.verifUserVocation,
+  userMiddleware.verifUserPassword,
+  userMiddleware.verifUserLevel,
+
+  userController.adicUser,
+
+);
 
 export default userRouter;
